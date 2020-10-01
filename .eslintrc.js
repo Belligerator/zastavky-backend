3b6ed9f0ea747f9046cde824998b866aacaef596
Dmitrij Bučkovskij <dmitrij.buckovsky@gmail.com>
2020-10-01 12:31:47 +0200

    KOnfiguracni soubory

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,36 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
     '@typescript-eslint/no-explicit-any': 'off',
-  },
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-inferrable-types': 'off',
+    'indent': ['error', 4, { 'SwitchCase': 1 }],
+    'quotes': ['error', 'single'],
+    'object-curly-spacing': ['error', 'always'],
+    'array-bracket-spacing': ['error', 'never'],
+    '@typescript-eslint/typedef': [
+      'warn',
+      {
+        'variableDeclaration': true,
+        'arrayDestructuring': true,
+        'memberVariableDeclaration': true,
+        'objectDestructuring': true,
+        'parameter': false,
+        'propertyDeclaration': true,
+        'variableDeclarationIgnoreFunction': true,
+        'arrowParameter': false,
+      },
+    ],
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      {
+        accessibility: 'explicit',
+        overrides: {
+          constructors: 'no-public',
+        },
+      },
+    ],
+  }
 };
