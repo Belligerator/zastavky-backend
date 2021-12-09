c501671b4741c4e97f5ed07316abfb4a14fa641b
Dmitrij Buckovsky <bucicovschii@gmail.com>
2021-12-09 13:47:55 +0100

    Zkouska ssl cert

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync('./server_key.pem'),
+  cert: fs.readFileSync('./server_cert.pem'),
+};
 
 async function bootstrap() {
-    const app: any = await NestFactory.create(AppModule);
+const app: any = await NestFactory.create(AppModule, {httpsOptions});
     app.setGlobalPrefix('api');
     app.enableCors();
     await app.listen(3000);
